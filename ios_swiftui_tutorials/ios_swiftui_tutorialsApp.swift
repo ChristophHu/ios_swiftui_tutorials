a797c9d64d2a4803edd88a0317768bdb18f4368f
christophhu <christoph.huschenhoefer@gmx.de>
2025-08-11 08:17:44 +0200

    Initial Commit

---
+++ ios_swiftui_tutorials/ios_swiftui_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftui_tutorialsApp.swift
+//  ios_swiftui_tutorials
+//
+//  Created by Christoph Huschenhöfer on 11.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftui_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
